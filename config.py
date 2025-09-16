d36fde5828623b9d0dcc70a5024ec6f7167b446b
Streamtg <202310475@issu.edu.do>
2025-09-16 13:46:48 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,5 @@
 # MIT License
-
 # Copyright (c) 2022 Hash Minner
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE
-
-# edit this file with your veriable if you'r deploy bot via locally | vps
 
 from dotenv import load_dotenv
 import os
@@ -29,24 +8,23 @@ load_dotenv()
 
 class Config(object):
 
-    # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "xxxxadd")
-    # bot username without @
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "xxxxadd")
+    # Get a token from @BotFather
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME")
 
     # Get these values from my.telegram.org
-    API_ID = os.environ.get("API_ID", "123add")
-    API_HASH = os.environ.get("API_HASH", "xxxxadd")
+    API_ID = int(os.environ.get("API_ID", 0))
+    API_HASH = os.environ.get("API_HASH")
 
     # TG Ids
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "123add")
-    OWNER_ID = os.environ.get("OWNER_ID", "123add")
-    AUTH_USERS = [OWNER_ID] + os.environ.get("AUTH_USERS", "").split(" ")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+    AUTH_USERS = [OWNER_ID] + [int(x) for x in os.environ.get("AUTH_USERS", "").split() if x]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
 
     # No need to change
     ADL_BOT_RQ = {}
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS")
-    CHUNK_SIZE = os.environ.get("CHUNK_SIZE", 128)
-    TG_MAX_FILE_SIZE = os.environ.get("TG_MAX_FILE_SIZE", 4194304000)
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    TG_MAX_FILE_SIZE = int(os.environ.get("TG_MAX_FILE_SIZE", 4194304000))
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
-    PROCESS_MAX_TIMEOUT = os.environ.get("PROCESS_MAX_TIMEOUT", 3700)
+    PROCESS_MAX_TIMEOUT = int(os.environ.get("PROCESS_MAX_TIMEOUT", 3700))
